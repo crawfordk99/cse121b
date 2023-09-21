0df2e256f44b060804b5f738802f27ef77d0bc71
Keith Crawford <keithacrawfordjr@gmail.com>
2023-09-20 19:36:58 -0500

    more practice

---
+++ dom-basics.js
@@ -0,0 +1,6 @@
+const newParagraph= document.createElement('p');
+newParagraph.innerText= 'Added with JavaScript!';
+document.body.appendChild(newParagraph)
+const sect = document.createElement('section');
+sect.innerHTML= "<h2>CSE 121B</h2> <p>Welcome to JavaScript Language</p>";
+document.body.appendChild(sect);
---
+++ practice.js
@@ -0,0 +1,20 @@
+let x = 2+3;
+console.log(x);
+x = 2*3;
+console.log(x);
+x = x-1;
+console.log(x);
+const to = "to";
+const the = "the";
+const string = to + the;
+console.log(string);
+let y = 5;
+console.log(y);
+y += 3;
+console.log(y);
+console.log(x === y);
+console.log(x < y);
+console.log(x !== y);
+console.log(x < 10 && x > 0);
+
+
